a3d03205e03359883f35f812154673756afb8c73
david molina <davidmolina258@gmail.com>
2022-06-23 15:30:50 -0300

    ordenando mis notas y añadiendo nuevos metodos de arrays

--- app.js
+++ app.js
@@ -20,20 +20,29 @@ const posts = [
   },
 ];
 
+// *******************************para transformar datos en nuevos arrays tenemos Array.from() o varibale.split()
+// 1.- a partir del objeto array
 let array = Array.from(texto);
 console.log(array);
+// 2.- con el metodo split()
 let array2 = texto.split(" ");
+console.log(array2);
+// 3.- los cuatro metodos basicos de los Arrays push pop unshift y shift
 array2.push("agregado al final");
 array2.pop();
 array2.unshift("agregado al comienzo");
 array2.shift();
 console.log(array2);
 
+// ********************************estructura basica para recorrer un array
+
 // for (let i = 0; i < numeros.length; i++) {
 //   const element = numeros[i];
 //   let result = element + 2;
 //   console.log(result);
 // }
+
+// **********************************Metodos avanzados de los Arrays
 
 // 1.- metodo Filter: es un callback que recibe como parametro el (Element, index y array),  utilizando una condicion podemos filtrar elementos de un array y devolver un NUEVO ARRAY con los elementos que cumplan la condicion
 const result = numeros.filter((el) => el > 3 && el < 8);
@@ -71,3 +80,32 @@ console.log(palabraExistente);
 
 let existeEnTodos = posts.every((el) => el.post.includes("lorem"));
 console.log(existeEnTodos);
+
+//8.- Metodo sort(), metodo que ordena el arreglo, cuando tenemos strings lo hace alfabeticamente, cuando tenemos number lo hace de menor a mayor; en el caso particular de los numeros se trabaja con un factor de comparacion. sort() recibe como parametro una funcion de comparacion
+
+let frutas = ["cereza", "banana", "anana", "frambuesa", "durazno"];
+let desorden = [7, 9, 34, 95, 83, 10, 110, 135];
+
+function comparacion(a, b) {
+  return a - b;
+  // si a-b =-1; a se situa en un indice menor
+  // si a-b =1; b se situa en un indice menor
+  // si a-b=0; no hay cambios
+}
+frutas.sort();
+console.log(frutas);
+desorden.sort(comparacion);
+console.log(desorden);
+
+// 9.- metodo reduce:  recibe dos parametros; una callback que a diferencia de los demas recibe 4 parametros Element, index, array y el valorPrevio=acumulador; en este acumulador se guarda el resultado de las iteraciones que se van haciendo.
+// elsugundo parametro de reduce es un valor inicial
+
+let sumaTodos = numeros.reduce((acc, el) => {
+  return acc + el;
+}, 0);
+console.log(sumaTodos);
+
+let saludar = array2.reduce((acc, el) => {
+  return (acc += " " + el);
+});
+console.log(saludar);
